836bd4bdd381b5212c3afbe8442a514d1e6d60d8
Michael Akinyemi <akinsmikopee2507@gmail.com>
2025-11-02 07:46:31 +0100

    Setup App.jsx with useState and addTodo logic

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -1,35 +1,37 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
 
-function App() {
-  const [count, setCount] = useState(0);
+const App = () => {
+  // State to hold all todos
+  const [todos, setTodos] = useState([]);
+
+  // function to add a new todo
+  const addTodo = (newTodo) => {
+    // ignore empty input
+    if (newTodo.trim() === "") return;
+
+    // create new todo items
+    const todoItem = {
+      id: Date.now(), //unique ID
+      text: newTodo,
+    };
+
+    // add to the list
+    setTodos([todoItem, ...todos]);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="min-h-screen bg-gray-100 flex flex-col item-center p-6">
+      <h1 className="text-3xl font-bold text-blue-600 mb-6">My Todo List</h1>
+
+      {/* input section */}
+      <TodoInput addTodo={addTodo} />
+
+      {/* todo list section */}
+      <TodoList todos={todos} />
+    </div>
   );
-}
+};
 
 export default App;
