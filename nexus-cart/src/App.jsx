b923fde31244c27bb6ec7425514d7bd3e1618d4d
Michael Akinyemi <akinsmikopee2507@gmail.com>
2025-12-20 19:00:57 +0100

    built the components with placeholder contents

--- nexus-cart/src/App.jsx
+++ nexus-cart/src/App.jsx
@@ -1,13 +1,25 @@
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/"></Route>
-        <Route></Route>
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <Navbar />
+
+      <main>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/products" element={<Products />} />
+          </Routes>
+        </BrowserRouter>
+      </main>
+
+      <Footer />
+    </div>
   );
 };
 
