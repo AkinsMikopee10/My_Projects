9252c5d2d4e1b076f17ada739ef5233368fa415e
Michael Akinyemi <akinsmikopee2507@gmail.com>
2025-12-20 19:06:51 +0100

    setup React + Tailwind project with base structure

---
+++ nexus-cart/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <h1>NexusCart</h1>
+      <div>
+        <a href="/">Home</a>
+        <a href="/products">Products</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
