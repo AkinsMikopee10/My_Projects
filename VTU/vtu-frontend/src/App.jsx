2bdaa0f4eb2891bbcf123868eb6a4d15276f7818
Michael Akinyemi <akinsmikopee2507@gmail.com>
2026-02-16 19:31:15 +0100

    added user registration page and connect to backend API

--- VTU/vtu-frontend/src/App.jsx
+++ VTU/vtu-frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
       <div className="w-full max-w-md bg-white min-h-screen">
         <Routes>
           <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
           <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </div>
