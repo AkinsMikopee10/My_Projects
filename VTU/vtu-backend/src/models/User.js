2bd1285e39168316d1ba0b8dc9866734af2e8191
Michael Akinyemi <akinsmikopee2507@gmail.com>
2026-02-16 19:50:26 +0100

    added balance field to user model

--- VTU/vtu-backend/src/models/User.js
+++ VTU/vtu-backend/src/models/User.js
@@ -16,13 +16,17 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    balance: {
+      type: Number,
+      default: 0,
+    },
     role: {
       type: String,
       enum: ["user", "admin"],
       default: "user",
     },
   },
-  { timestamps: true }
+  { timestamps: true },
 );
 
 export default mongoose.model("User", userSchema);
