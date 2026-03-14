896f79f74789e354ed0a31acc34acf32c4905b44
Michael Akinyemi <akinsmikopee2507@gmail.com>
2026-03-14 14:32:16 +0100

    created new Admin middleware, updated admin routes and removed CORS from server.js

--- VTU/vtu-backend/src/routes/admin.routes.js
+++ VTU/vtu-backend/src/routes/admin.routes.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { fundWallet } from "../controllers/admin.controller.js";
-import { updateDataPlan } from "../controllers/admin.controller.js";
+import { fundWallet, updateDataPlan } from "../controllers/admin.controller.js";
 import authMiddleware from "../middleware/auth.middleware.js";
+import isAdmin from "../middleware/isAdmin.middleware.js";
 
 const router = express.Router();
 
-router.post("/fund-wallet", fundWallet);
-router.patch("/data-plan/:id", authMiddleware, updateDataPlan);
+router.post("/fund-wallet", authMiddleware, isAdmin, fundWallet);
+router.patch("/data-plan/:id", authMiddleware, isAdmin, updateDataPlan);
 
 export default router;
---
+++ VTU/vtu-frontend/src/pages/DataConfirm.jsx
@@ -0,0 +1,11 @@
+import { useLocation } from "react-router-dom";
+
+const DataConfirm = () => {
+  return (
+    <div>
+      <h1>DataConfirm</h1>
+    </div>
+  );
+};
+
+export default DataConfirm;
