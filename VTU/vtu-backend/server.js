0132c1892c8a3d914925813553f12132cc54f463
Michael Akinyemi <akinsmikopee2507@gmail.com>
2026-03-14 14:32:16 +0100

    created new Admin middleware, updated admin routes and removed CORS from server.js

--- VTU/vtu-backend/server.js
+++ VTU/vtu-backend/server.js
@@ -1,6 +1,5 @@
 import app from "./src/app.js";
 import connectDB from "./src/config/db.js";
-import cors from "cors";
 
 const PORT = process.env.PORT || 5000;
 
@@ -9,10 +8,3 @@ connectDB();
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  }),
-);
